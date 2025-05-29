117629d533b931dbedbe8971f6f382d4aead6365
amo-chamo <koranashvili.amiran@gmail.com>
2025-05-29 14:30:16 +0400

    Initialize Spring Boot project with Maven configuration

---
+++ src/main/java/org/amochamo/fluqmusicmarket/FluqMusicMarketApplication.java
@@ -0,0 +1,13 @@
+package org.amochamo.fluqmusicmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluqMusicMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FluqMusicMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/amochamo/fluqmusicmarket/FluqMusicMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package org.amochamo.fluqmusicmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluqMusicMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
